6ec7deb860d41bf944f8886d960141da0a0485f8
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-01-18 20:28:03 -0800

    Separated into Components
    Separated the Frame from main into its own component

---
+++ GUI-Version1/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+	public static void main(String[] args) {
+		//JFrame = a GUI window to add components to
+		
+		/*
+		JFrame frame = new JFrame(); //creates a frame
+		frame.setTitle("JFrame title goes here"); //sets the title of the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+		frame.setResizable(false); //prevent frame from being resized
+		frame.setSize(420,420); //sets the x-dimension, and y-dimension of frame
+		frame.setVisible(true); //make frame visible
+		
+		ImageIcon image = new ImageIcon("flower-icon-png.png"); //create a ImageIcon
+		frame.setIconImage(image.getImage()); //change icon of frame
+		frame.getContentPane().setBackground(new Color(255,255,255)); //change color of background
+		*/
+		
+		MyFrame myFrame = new MyFrame();
+		
+	}
+}
